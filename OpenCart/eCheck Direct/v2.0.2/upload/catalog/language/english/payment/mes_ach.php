dcfcfd8172ae1a7aab6b46895ccf73f3e204119e
Nate Richardson <nate@richardsonmediahouse.com>
2015-05-09 00:56:47 -0700

    removed archives, removed beta features from opencart plugins and updated with minor usability tweaks

--- OpenCart/eCheck Direct/v2.0.2/upload/catalog/language/english/payment/mes_ach.php
+++ OpenCart/eCheck Direct/v2.0.2/upload/catalog/language/english/payment/mes_ach.php
@@ -2,7 +2,7 @@
 /**
 * Custom Payment Gateway for OpenCart v2
 * Using Payment Gateway API v4.12
-* For Credit Card Transactions
+* For ACH Transactions
 * Written 09/20/2014
 * ©Merchant e-Solutions 2014
 *
@@ -11,13 +11,13 @@
 */
 
 // Text
-$_['text_title']           = 'Credit or Debit Card';
-$_['text_credit_card']     = 'Credit Card Details';
+$_['text_title']           = 'eCheck Direct';
+$_['text_ach']             = 'eCheck Details';
 $_['text_wait']            = 'Please wait!';
 $_['text_testmode']        = 'Warning: The payment gateway is in \'Sandbox Mode\'. Your account will not be charged.';
 
 // Entry
-$_['entry_cc_number']      = 'Card Number:';
-$_['entry_cc_expire_date'] = 'Card Expiry Date:';
-$_['entry_cc_cvv2']        = 'Card Security Code (CVV2):';
+$_['entry_ach_tran']       = 'Routing Number:';
+$_['entry_ach_acct']       = 'Account Number:';
+$_['entry_ach_type']       = 'Account Type:';
 ?>
